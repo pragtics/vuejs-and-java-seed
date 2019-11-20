af8fda2fac916a9df8422761b18abeb29df76413
Jan Bartkowiak <jan.bartkowiak@pragtics.de>
2019-11-20 17:37:41 +0100

    Initial commit

---
+++ backend/src/main/java/de/pragtics/seeds/java/springbootbackend/SpringBootBackendApplication.java
@@ -0,0 +1,15 @@
+package de.pragtics.seeds.java.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApplication {
+
+	private static int test;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApplication.class, args);
+	}
+
+}
