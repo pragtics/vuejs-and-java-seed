2656d12a7eef4b9513b7ca85eab974d157ca923f
Jan Bartkowiak <jan.bartkowiak@pragtics.de>
2019-12-01 10:39:44 +0100

    - Adapted port for local development.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,9 +4,10 @@ module.exports = {
         "vuetify"
     ],
     devServer: {
+        port: 8088,
         proxy: {
             "^/api": {
-                target: "http://localhost:8088",
+                target: "http://localhost:8080",
                 ws: true,
                 changeOrigin: true
             }
