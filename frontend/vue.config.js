27dc438bdad8dfcb6abbaf6c1ef69af1a851d5c3
Jan Bartkowiak <jan.bartkowiak@pragtics.de>
2019-12-01 10:26:49 +0100

    - Added generated Vue.js sources to Spring Boot application. - Did some minor clean-ups.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    outputDir: 'dist/public',
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    devServer: {
+        proxy: {
+            "^/api": {
+                target: "http://localhost:8088",
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
 }
